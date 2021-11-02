d5038895c33ec8ad10fe6cd5abca2d6e0b0d0f1f
vojakja <jkaub.vojak@student.uzlabina.cz>
2021-11-02 16:25:45 +0100

    CookiesClickerStart

--- app/src/main/java/com/StudioNeko/Appka/MainActivity.kt
+++ app/src/main/java/com/StudioNeko/Appka/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.StudioNeko.Appka
 
+import android.graphics.Color
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,19 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.constraintlayout.motion.utils.ViewOscillator
 import com.StudioNeko.Appka.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val PrimeTextSize = 20f
+    private val NonPrimeTextSize = 11f
+    private var cookies = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        btn_1.setOnClickListener {
+            //CheckNumber()
+        }
+
+        btn_2.setOnClickListener {
+            cookies++
+        }
+        AutoPlay()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -38,6 +55,58 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun CheckNumber(){
+        if(PrimeNumber(NumberInput.text.toString().toInt())){
+            PrimeTextView.text = "Number is Prime and its position is " + WhichPrimeNumber(NumberInput.text.toString().toInt())
+            PrimeTextView.setTextColor(Color.RED)
+            PrimeTextView.textSize = PrimeTextSize
+        } else{
+            PrimeTextView.text = "Number is not Prime"
+            PrimeTextView.setTextColor(Color.GREEN)
+            PrimeTextView.textSize = NonPrimeTextSize
+        }
+    }
+
+    fun PrimeNumber(a: Int): Boolean{
+        var flags = 0
+        if(a == 1)
+            return false
+
+        for(i in 1..a){
+            if(a % i == 0){
+                flags++
+            }
+            if(flags > 2){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun WhichPrimeNumber(a: Int): Int{
+        var position = 0
+        for(i in 2..a){
+            var index = 0
+            for(j in 1..i/2){
+                if(i % j == 0){
+                    index++
+                }
+            }
+            if(index < 2){
+                position++
+            }
+        }
+        return position
+    }
+
+    fun AutoPlay(){
+        PrimeTextView.text = cookies.toString()
+        cookies++
+            Timer("SettingUp", false).schedule(500){
+            AutoPlay()
         }
     }
 }
